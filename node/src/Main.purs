8001fd26f996578a5c6083b1978ed393e157d16f
Yannick Gottschalk <ygottschalk@techfak.uni-bielefeld.de>
2019-11-05 22:56:14 +0100

    added install, build and run scripts

--- node/src/Main.purs
+++ node/src/Main.purs
@@ -1,16 +1,24 @@
 module Main where
 
 import Prelude hiding (apply)
+import Data.Maybe (fromMaybe)
+import Data.Int (fromString)
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Express.App (App, listenHttp, get)
 import Node.Express.Response (send)
 import Node.HTTP (Server)
+import Node.Process (lookupEnv)
+
+
+parseInt :: String -> Int
+parseInt str = fromMaybe 0 $ fromString str
 
 app :: App
 app = get "/" $ send "Hello, World!"
 
 main :: Effect Server
 main = do
-    listenHttp app 8080 \_ ->
-        log $ "Listening on " <> show 8080
+  port <- (parseInt <<< fromMaybe "8080") <$> lookupEnv "PORT"
+  listenHttp app port \_ ->
+      log $ "Listening on " <> show port
