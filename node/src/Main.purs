754624461974222b512c6c985390a1c598021726
Yannick Gottschalk <ygottschalk@techfak.uni-bielefeld.de>
2019-11-05 20:35:52 +0100

    purescript and nodejs setup complete

--- node/src/Main.purs
+++ node/src/Main.purs
@@ -1,9 +1,16 @@
 module Main where
 
-import Prelude
+import Prelude hiding (apply)
 import Effect (Effect)
 import Effect.Console (log)
+import Node.Express.App (App, listenHttp, get)
+import Node.Express.Response (send)
+import Node.HTTP (Server)
 
-main :: Effect Unit
+app :: App
+app = get "/" $ send "Hello, World!"
+
+main :: Effect Server
 main = do
-  log "Hello sailor!"
+    listenHttp app 8080 \_ ->
+        log $ "Listening on " <> show 8080
