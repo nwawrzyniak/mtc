7bfed81475092c7008351a1265c9106d45f32b0a
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-12-01 07:55:00 +0100

    Updated a line in full_run.sh to match newest version of build.sh

--- full_run.sh
+++ full_run.sh
@@ -4,5 +4,5 @@ cd node
 npm install
 bower install
 pulp build
-pulp browserify -m FrontendMain -O -t static/main.js
+pulp browserify -m FrontendMain -O | npx uglifyjs --compress --mangle --toplevel -o static/main.min.js
 PORT="${PORT:-8080}" pulp run
